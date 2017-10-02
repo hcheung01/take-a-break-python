1772e7150c8fb9dcbc8aeee79d20dc78e2bf07d8
Heindrick <hcheung01@gmail.com>
2017-10-02 01:02:41 -0700

    final copy

--- take-a-break.py
+++ take-a-break.py
@@ -6,6 +6,6 @@ break_count = 0
 
 print("This program started on "+time.ctime())
 while(break_count < total_breaks):
-    time.sleep(2)
+    time.sleep(120)
     webbrowser.open('https://www.youtube.com/watch?v=tVeS4dKa3E4')
     break_count = break_count + 1
